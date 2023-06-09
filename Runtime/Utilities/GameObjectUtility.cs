48ece87c6ecc80dbac084ed4cb620cd6dd5a83fa
IVProject <slimprod.game@gmail.com>
2023-06-10 00:28:08 +0300

    Added: new utility

---
+++ Runtime/Utilities/GameObjectUtility.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Mud.Utilities
+{
+    public static class GameObjectUtility
+    {
+        public static T Create<T>(string name, Transform parent, Vector3 position, Quaternion rotation) where T: Component
+        {
+            var obj = new GameObject(name);
+            obj.transform.SetPositionAndRotation(position, rotation);
+            obj.transform.SetParent(parent);
+            
+            var component = obj.AddComponent<T>();
+            
+            return component;
+        }
+        
+        public static T Create<T>(string name, Transform parent) where T : Component
+        {
+            return Create<T>(name, parent, Vector3.zero, Quaternion.identity);
+        }
+        
+        public static T Create<T>(string name) where T : Component
+        {
+            return Create<T>(name, null, Vector3.zero, Quaternion.identity);
+        }
+        
+        public static GameObject Create(string name, Transform parent, Vector3 position, Quaternion rotation) 
+        {
+            var obj = new GameObject(name);
+            obj.transform.SetPositionAndRotation(position, rotation);
+            obj.transform.SetParent(parent);
+
+            return obj;
+        }
+
+        public static GameObject Create(string name, Transform parent)
+        {
+            return Create(name, parent, Vector3.zero, Quaternion.identity);
+        }
+        
+        public static GameObject Create(string name)
+        {
+            return Create(name, null, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
